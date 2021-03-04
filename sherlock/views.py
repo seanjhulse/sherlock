f5926f4c7a8917d227a1b0dde4af58c3a03b4240
Giovanni Flores <gdr943@gmail.com>
2021-03-03 18:40:24 -0800

    Improved upon existing os detection

--- sherlock/views.py
+++ sherlock/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils import timezone
+from sherlock.models import Packet
+
 import socket
 from .osdata import list_os
 
@@ -42,21 +45,20 @@ def network_traffic(request, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
 
     # recieve packet
-    packet = sock.recvfrom(port)
-    
-    # Get data from the packet tuple
-    data = packet[0]
-
-    # Get IP Address from the packet tuple
-    ip_address = packet[1][0]
+    raw_packet = sock.recvfrom(port)
 
     # Close the socket
     sock.close()
+    
+    # Get data from the packet tuple
+    data = raw_packet[0]
+
+    # Get IP Address from the packet tuple
+    host_ip_address = raw_packet[1][0]
+
+    # Create a packet object and save it to the database
+    packet = Packet(host_ip_address=host_ip_address, dest_ip_address="localhost", port=port, payload=data, pub_date=timezone.now())
+    packet.save()
 
     # Return the data in a basic HttpResponse
-    return HttpResponse("You're getting network traffic %s on port %s:%s" % (data, ip_address, port))
-
-def network_operating_systems(request):
-    os_dict = list_os()
-    
-    return HttpResponse("The OS on this network are: %s" % (os_dict))
+    return HttpResponse("You're getting network traffic %s on port %s:%s" % (packet.payload, packet.host_ip_address, packet.port))
