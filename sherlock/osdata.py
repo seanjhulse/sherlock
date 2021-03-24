26f2f3e38797dbd7b08616fbe68c14198b9c9818
Giovanni Flores <gdr943@gmail.com>
2021-03-23 17:27:34 -0700

    made localhost page displaying other hosts

--- sherlock/osdata.py
+++ sherlock/osdata.py
@@ -1,22 +1,16 @@
 import nmap
+import nmap3 # if you don't have nmap3, either run pip install python3-nmap or just comment this line and list_os() method out
 import os
 import re
+import socket
+import subprocess
+import multiprocessing
 
 
-def get_ipaddrs():
-    # arp -a normally maps ip addresses to mac addresses, but works for our purposes in listing all ips in LAN (including gateway)
-    devices = [device for device in os.popen('arp -a')]
-    devices.insert(0, '127.0.0.1')
-    ipaddrs = []
-    for device in devices:
-        # makes sure we only get the ipv4 and not rest of information
-        ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', device)    
-        ipaddrs.append(ip[0])
-    return ipaddrs
 
-
+# deprecated method,must refactor at a later date
 def list_os():
-    ips = get_ipaddrs()
+    ips = map_net()
     nmap3 = nmap.Nmap()
     ips_os = {}
     
@@ -31,3 +25,55 @@ def list_os():
         print(f'Ip address: {ip}, os info: {os_info}')
     
     return ips_os
+
+
+def pinger(workq, resq):
+    DEVNULL = open(os.devnull, 'w')
+    while True:
+        ip = workq.get()
+        if ip is None:
+            break
+        try:
+            subprocess.check_call(['ping', '-c1', ip], stdout=DEVNULL)
+            resq.put(ip)
+        except:
+            pass
+
+def map_net(pool_size=255):
+    ip_list = []
+    ip_parts = get_ip().split('.')
+    base_ip = ip_parts[0] + '.' + ip_parts[1] + '.' + ip_parts[2] + '.'
+    jobs = multiprocessing.Queue()
+    results = multiprocessing.Queue()
+    pool = [multiprocessing.Process(target=pinger, args=(jobs, results)) for i in range(pool_size)]
+    for p in pool:
+        p.start()
+    for i in range(1, 255):
+        jobs.put(base_ip + '{0}'.format(i))
+    for p in pool: 
+        jobs.put(None)
+    for p in pool:
+        p.join()
+    while not results.empty():
+        ip = results.get()
+        ip_list.append(ip)
+    return ip_list
+
+def get_op_sys(ip_list):
+    scanner = nmap.PortScanner()
+    res = []
+    for ip in ip_list:
+        res.append(scanner.scan(ip, arguments="-O")['scan'])
+    
+    return res
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.connect(('10.255.255.255', 1))
+        ip = s.getsockname()[0]
+    except Exception:
+        ip = '127.0.0.1'
+    finally: 
+        s.close()
+    return ip
