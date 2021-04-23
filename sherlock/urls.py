67a1d3a0b4b64108abe8ca9bfd49fb1a38340c7b
blake <blake.pinette@gmail.com>
2021-04-23 14:19:22 -0700

    fix issues introduced during my last pr

--- sherlock/urls.py
+++ sherlock/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import include, url
 
 from . import views
 
+
 urlpatterns = [
     path('', views.index, name='index'),
     # ex: /example/5/
@@ -12,8 +13,8 @@ urlpatterns = [
     path('localpage/<str:ajaxip>', views.localpage, name='localpage'),
     path('portpage/<str:ajaxip>', views.portpage, name='portpage'),
     path('network-os/<str:ip>', views.network_operating_systems, name='network-os'),
-	  path('host-scan-all/<str:ipaddress>/', views.host_scan_all, name='host-scan-all'),
-	  path('host-scan/<str:ipaddress>/<str:portrange>', views.host_scan, name='host-scan'),
+	path('host-scan-all/<str:ipaddress>/', views.host_scan_all, name='host-scan-all'),
+	path('host-scan/<str:ipaddress>/<str:portrange>', views.host_scan, name='host-scan'),
     path('local-ports/', views.local_ports, name="local-ports"),
     path('node-map', views.node_map, name='node-map'),
     path('host-node', views.host_node, name='host-node'),
